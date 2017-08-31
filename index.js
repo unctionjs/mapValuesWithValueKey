967336563ee2e67d4248c079026656b722df1ed7
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-31 12:08:27 -0700

    Bad type, plus removing the need to determine what we're mapping

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 import attach from "@unction/attach"
-import isArray from "@unction/isarray"
-import isObject from "@unction/isobject"
+import fresh from "@unction/fresh"
 import reduceWithValueKey from "@unction/reducewithvaluekey"
 
-export default function mapValuesWithValueKey (unction: ValueType => ValueType): Function {
+export default function mapValuesWithValueKey (unction: ValueType => KeyType => ValueType): Function {
   return function mapValuesWithValueKeyUnction (functor: FunctorType): FunctorType {
-    const reducedWithUnction = reduceWithValueKey(
-      (accumulated: AccumulatedType): Function => (value: ValueType): Function => (key: KeyType): FunctorType => attach(key)(unction(value)(key))(accumulated)
+    return reduceWithValueKey(
+      (accumulated: AccumulatedType): Function =>
+        (value: ValueType): Function =>
+          (key: KeyType): FunctorType =>
+            attach(key)(unction(value)(key))(accumulated)
+    )(
+      fresh(functor)
+    )(
+      functor
     )
-
-    if (isArray(functor)) {
-      return reducedWithUnction(
-        []
-      )(
-        functor
-      )
-    }
-
-    if (isObject(functor)) {
-      return reducedWithUnction(
-        {}
-      )(
-        functor
-      )
-    }
-
-    throw new Error("Couldn't figure out how to map over this functor")
   }
 }
