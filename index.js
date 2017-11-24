9bce7dc578e3a9cd8248be697c30682e86838b5d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-11-24 13:42:54 -0800

    Better types

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import attach from "@unction/attach"
 import fresh from "@unction/fresh"
 import reduceWithValueKey from "@unction/reducewithvaluekey"
 
-export default function mapValuesWithValueKey (unction: ValueType => (ObjectKeyType | ArrayKeyType | MapKeyType | ArrayKeyType | null) => ValueType): Function {
-  return function mapValuesWithValueKeyUnction (functor: ObjectType | ArrayType | MapType | StringType | SetType): ObjectType | ArrayType | MapType | StringType | SetType {
+export default function mapValuesWithValueKey (unction: ValueType => (KeyType | null) => ValueType): UnaryFunctionType {
+  return function mapValuesWithValueKeyUnction (functor: FunctorType): FunctorType {
     return reduceWithValueKey(
-      (accumulated: ObjectType | ArrayType | MapType | StringType | SetType): Function =>
-        (value: ValueType): Function =>
-          (key: (KeyType | null)): ObjectType | ArrayType | MapType | StringType | SetType =>
+      (accumulated: FunctorType | StreamType): UnaryFunctionType =>
+        (value: ValueType): UnaryFunctionType =>
+          (key: (KeyType | null)): FunctorType =>
             attach(key)(unction(value)(key))(accumulated)
     )(
       fresh(functor)
