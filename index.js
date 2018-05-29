388fc6df2159e0749ad895ba9b7f53b09389d749
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:30:06 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-import mergeRight from "@unction/mergeright"
-import fresh from "@unction/fresh"
-import of from "@unction/of"
-import reduceWithValueKey from "@unction/reducewithvaluekey"
-
-export default function mapValuesWithValueKey (unction: ValueType => (KeyType | void) => ValueType): UnaryFunctionType {
-  return function mapValuesWithValueKeyUnction (functor: FunctorType): FunctorType {
+import mergeRight from "@unction/mergeright";
+import fresh from "@unction/fresh";
+import of from "@unction/of";
+import reduceWithValueKey from "@unction/reducewithvaluekey";
+export default function mapValuesWithValueKey(unction) {
+  return function mapValuesWithValueKeyUnction(functor) {
     if (functor.map instanceof Function) {
-      return functor.map((value: ValueType, key: KeyType | void): ValueType => unction(value)(key))
+      return functor.map((value, key) => unction(value)(key));
     }
 
-    return reduceWithValueKey(
-      (accumulated: FunctorType): UnaryFunctionType =>
-        (value: ValueType): UnaryFunctionType =>
-          (key: (KeyType | void)): FunctorType => {
-            return mergeRight(accumulated)(of(key)(unction(value)(key))(accumulated))
-          }
-    )(
-      fresh(functor)
-    )(
-      functor
-    )
-  }
+    return reduceWithValueKey(accumulated => value => key => {
+      return mergeRight(accumulated)(of(key)(unction(value)(key))(accumulated));
+    })(fresh(functor))(functor);
+  };
 }
