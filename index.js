5606a6a960fcbfe5d62dff8ebd96d0f305313c45
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:44 -0700

    Autolinting

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import mergeRight from "@unction/mergeright";
 import fresh from "@unction/fresh";
 import of from "@unction/of";
 import reduceWithValueKey from "@unction/reducewithvaluekey";
-export default function mapValuesWithValueKey(unction) {
-  return function mapValuesWithValueKeyUnction(functor) {
+export default function mapValuesWithValueKey (unction) {
+  return function mapValuesWithValueKeyUnction (functor) {
     if (functor.map instanceof Function) {
       return functor.map((value, key) => unction(value)(key));
     }
 
-    return reduceWithValueKey(accumulated => value => key => {
+    return reduceWithValueKey((accumulated) => (value) => (key) => {
       return mergeRight(accumulated)(of(key)(unction(value)(key))(accumulated));
     })(fresh(functor))(functor);
   };
--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 /* eslint-disable flowtype/require-return-type */
-import {test} from "tap"
-import {of} from "most"
-import streamSatisfies from "@unction/streamsatisfies"
+import {test} from "tap";
+import {of} from "most";
+import streamSatisfies from "@unction/streamsatisfies";
 
-import mapValuesWithValueKey from "./index"
+import mapValuesWithValueKey from "./index";
 
 test("Array", ({same, end}) => {
   same(
     mapValuesWithValueKey((value) => (index) => `${value}:${index}`)(["a", "b", "c"]),
     ["a:0", "b:1", "c:2"]
-  )
-  end()
-})
+  );
+  end();
+});
 
 test("Object", ({same, end}) => {
   same(
@@ -25,33 +25,33 @@ test("Object", ({same, end}) => {
       bbb: "b:bbb",
       ccc: "c:ccc",
     }
-  )
-  end()
-})
+  );
+  end();
+});
 
 test("Set", ({same, end}) => {
   same(
     mapValuesWithValueKey((value) => (index) => `${value}:${index}`)(new Set(["a", "b", "c"])),
     new Set(["a:undefined", "b:undefined", "c:undefined"])
-  )
-  end()
-})
+  );
+  end();
+});
 
 test("Map", ({same, end}) => {
   same(
     mapValuesWithValueKey((value) => (index) => `${value}:${index}`)(new Map([["0", "a"], ["1", "b"], ["2", "c"]])),
     new Map([["0", "a:0"], ["1", "b:1"], ["2", "c:2"]])
-  )
-  end()
-})
+  );
+  end();
+});
 
 test("String", ({equal, end}) => {
   equal(
     mapValuesWithValueKey((value) => (index) => `${value}:${index}`)("abc"),
     "a:0b:1c:2"
-  )
-  end()
-})
+  );
+  end();
+});
 
 test("Stream", ({same, doesNotThrow, equal, end}) => {
   streamSatisfies(
@@ -65,8 +65,8 @@ test("Stream", ({same, doesNotThrow, equal, end}) => {
   )(
     ({length}) =>
       (position) => {
-        equal(length, position)
-        end()
+        equal(length, position);
+        end();
       }
   )(
     mapValuesWithValueKey(
@@ -76,5 +76,5 @@ test("Stream", ({same, doesNotThrow, equal, end}) => {
     )(
       of("a")
     ),
-  )
-})
+  );
+});
